f4ab1abf07e9ca49b68a155f6d6cd4401065ab64
unknown <ssameer>
2019-06-20 11:56:57 +0530

    adding beta as image script

---
+++ auth/build.bat
@@ -0,0 +1,21 @@
+
+
+set APPLICATION_NAME=auth
+set IMAGE_NAME=pss_%APPLICATION_NAME%
+set REGISTERGY_NAME=ssameer03
+set REPO_IMAGE=%REGISTERGY_NAME%/%IMAGE_NAME%:beta
+echo "> Cleaning target..."
+
+echo "> building jar..."
+
+call mvn package -Dmaven.test.skip=true
+
+echo "> Building %APPLICATION_NAME%:latest"
+docker login -u ssameer03 -pSameer@123
+docker build -t %IMAGE_NAME%:beta .
+
+docker tag pss_employee %REPO_IMAGE%
+
+docker push %REPO_IMAGE%
+echo "> Done."
+
