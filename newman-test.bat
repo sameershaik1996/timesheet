a3320c0b4f488fc9c48ba5354d43301a9b6fe68e
unknown <ssameer>
2019-06-21 16:12:36 +0530

    changed new man script

--- newman-test.bat
+++ newman-test.bat
@@ -1,5 +1,5 @@
 
-newman run "C:\Users\redshift\Desktop\TimeSheet.postman_collection" -e "C:\Users\redshift\Desktop\test.postman_environment.json"
+call newman run "C:\Users\redshift\Desktop\TimeSheet.postman_collection" -e "C:\Users\redshift\Desktop\test.postman_environment.json"
 
 
 docker stop employeeService database authService apiGateway serviceDiscovery timesheetService
