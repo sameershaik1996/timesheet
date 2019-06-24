5bd242ea8fccecd5b6b0eeca9c86ff967e7e0648
unknown <sameershaik03@gmail.com>
2019-06-24 13:54:59 +0530

    Jenkinsfile

--- newman-test.bat
+++ newman-test.bat
@@ -1,8 +1,8 @@
 
-call newman run "C:\Users\redshift\Desktop\TimeSheet.postman_collection" -e "C:\Users\redshift\Desktop\test.postman_environment.json"
+call newman run "C:\Users\redshift\Desktop\TimeSheet.postman_collection" -e "C:\Users\redshift\Desktop\test.postman_environment.json" --bail
 
+echo "hello 123"
+::docker stop employeeService database authService apiGateway serviceDiscovery timesheetService
+::docker rm employeeService database authService apiGateway serviceDiscovery timesheetService
 
-docker stop employeeService database authService apiGateway serviceDiscovery timesheetService
-docker rm employeeService database authService apiGateway serviceDiscovery timesheetService
-
-build-images.bat latest
+::build-images.bat latest
